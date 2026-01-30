682c5dd5d0b78ce875c0f22b8af5dee92801083f
xiang <569447332@qq.com>
2026-01-30 14:10:36 +0800

    'test'

---
+++ ruoyi-system/src/main/java/com/ruoyi/system/service/ISysNewsCategoryService.java
@@ -0,0 +1,61 @@
+package com.ruoyi.system.service;
+
+import com.ruoyi.system.domain.SysNewsCategory;
+import java.util.List;
+
+/**
+ * 新闻分类Service接口
+ *
+ * @author ruoyi
+ * @date 2026-01-30
+ */
+public interface ISysNewsCategoryService
+{
+    /**
+     * 查询新闻分类列表
+     *
+     * @param sysNewsCategory 新闻分类
+     * @return 新闻分类集合
+     */
+    public List<SysNewsCategory> selectSysNewsCategoryList(SysNewsCategory sysNewsCategory);
+
+    /**
+     * 查询新闻分类树形结构
+     *
+     * @param sysNewsCategory 新闻分类
+     * @return 新闻分类树形集合
+     */
+    public List<SysNewsCategory> selectSysNewsCategoryTree(SysNewsCategory sysNewsCategory);
+
+    /**
+     * 通过分类ID查询新闻分类
+     *
+     * @param categoryId 分类ID
+     * @return 新闻分类信息
+     */
+    public SysNewsCategory selectSysNewsCategoryById(Long categoryId);
+
+    /**
+     * 新增新闻分类
+     *
+     * @param sysNewsCategory 新闻分类
+     * @return 结果
+     */
+    public int insertSysNewsCategory(SysNewsCategory sysNewsCategory);
+
+    /**
+     * 修改新闻分类
+     *
+     * @param sysNewsCategory 新闻分类
+     * @return 结果
+     */
+    public int updateSysNewsCategory(SysNewsCategory sysNewsCategory);
+
+    /**
+     * 删除新闻分类信息
+     *
+     * @param categoryId 分类ID
+     * @return 结果
+     */
+    public int deleteSysNewsCategoryById(Long categoryId);
+}
